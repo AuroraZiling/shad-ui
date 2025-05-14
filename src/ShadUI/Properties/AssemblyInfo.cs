4277767417d5777c35e574efe0446cc51862e7ad
Artemis Li <auroraziling@gmail.com>
2025-05-14 19:19:41 +0800

    perf: ui adjustments

---
+++ src/ShadUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using Avalonia.Metadata;
+
+[assembly: XmlnsDefinition("clr-namespace:ShadUI.Controls;assembly=ShadUI", "ShadUI")]
+[assembly: XmlnsDefinition("clr-namespace:ShadUI.Controls;assembly=ShadUI", "ShadUI.Controls")]
+[assembly: XmlnsPrefix("clr-namespace:ShadUI.Controls;assembly=ShadUI", "shadui")]
